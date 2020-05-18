3042af2b4cebba2a00ef38a728ff803a8fba386e
Anselmo Ribeiro <6975.usa@gmail.com>
2020-05-18 15:21:45 -0400

    create main class

---
+++ src/main/java/com/allscontracting/Main.java
@@ -0,0 +1,13 @@
+package com.allscontracting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+	public static void main(String[] arguments) {
+		SpringApplication.run(Main.class, arguments);
+	}
+
+}
